2991ff6d17304b804fd0dd8bd339c973683b45c0
Dee Fisher <dekanifisher@yahoo.co.uk>
2020-08-11 18:26:21 +0100

    first commit

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [
+    "airbnb",
+    "@babel/preset-env", 
+    "@babel/preset-react",
+  ];
+  const plugins = [
+    "add-module-exports",
+    "@babel/transform-async-to-generator",
+    "@babel/plugin-proposal-function-bind",
+    ["transform-replace-object-assign", { "moduleSpecifier": "object.assign" }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+  ];
+
+  return {
+    presets,
+    plugins
+  };
+}
