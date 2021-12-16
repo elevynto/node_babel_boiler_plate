2ad8f067b7e614f2fae3bec701749b455d6db65d
Dee Fisher <dekanifisher@yahoo.co.uk>
2021-12-16 10:43:13 +0000

    included testing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-let customer = {
-    name: "Carl",
-    details: { age: 82 }
-  };
-  const customerCity = customer.city ?? "Unknown city";
-  console.log(customerCity); // Unknown city
+import url from 'url';
+const http = require('http');
+
+export let customer = {
+    name: 'Carl',
+    details: { age: 82 },
+};
+const customerCity = customer.city ?? 'Unknown city';
+console.log(customerCity); // Unknown city
