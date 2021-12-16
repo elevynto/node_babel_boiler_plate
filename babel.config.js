a722e4d0aed1cc6d14eb83d8a8647bee61791a11
Dee Fisher <dekanifisher@yahoo.co.uk>
2021-12-16 11:15:01 +0000

    cleaned up linting and babel

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,11 @@
-
 module.exports = function (api) {
-  api.cache(true);
+    api.cache(true);
 
-  const presets = [
-    "airbnb",
-    "@babel/preset-env", 
-    "@babel/preset-react",
-  ];
-  const plugins = [
-    "add-module-exports",
-    "@babel/transform-async-to-generator",
-    "@babel/plugin-proposal-function-bind",
-    "@babel/plugin-proposal-optional-chaining",
-    ["transform-replace-object-assign", { "moduleSpecifier": "object.assign" }],
-    ["@babel/plugin-proposal-class-properties", { "loose": true }],
-  ];
+    const presets = ['@babel/preset-env'];
+    const plugins = ['@babel/plugin-transform-modules-commonjs'];
 
-  return {
-    presets,
-    plugins
-  };
-}
+    return {
+        presets,
+        plugins,
+    };
+};
