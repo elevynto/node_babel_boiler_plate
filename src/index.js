b700eb554897205d965d2fa92119117275c33b2f
Dee Fisher <dekanifisher@yahoo.co.uk>
2021-12-16 10:47:40 +0000

    added jest settings

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import url from 'url';
 const http = require('http');
 
-=======
->>>>>>> f05205710798bec0f4cdafd3aef08a964d00a307
 export let customer = {
     name: 'Carl',
     details: { age: 82 },
